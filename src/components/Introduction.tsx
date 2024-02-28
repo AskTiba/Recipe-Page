1558d79b2b9d226dbd76d080e4d731e30466233c
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-02-29 01:05:56 +0300

    Create Introduction.tsx

---
+++ src/components/Introduction.tsx
@@ -0,0 +1,14 @@
+const Introduction = () => {
+  return (
+    <div className="text-dark-charcoal p-6">
+      <h1 className="mb-3 font-[600]">Simple Omelette Recipe</h1>
+      <p className="">
+        An easy and quick dish, perfect for any meal. This classic omelette
+        combines beaten eggs cooked to perfection, optionally filled with your
+        choice of cheese, vegetables, or meats.
+      </p>
+    </div>
+  );
+};
+
+export default Introduction;
